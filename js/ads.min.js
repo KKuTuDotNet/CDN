1f56244b46c26e1cc5a606549ca106805eccb33f
해티 <admin@hatty163.kr>
2020-05-26 23:09:48 +0900

    20.5.28

---
+++ js/ads.min.js
@@ -0,0 +1,8 @@
+/**
+Rule the Words!, AlphaKKuTu Online. ⓒ 2017-2019 AlphaKKuTu. All Rights Reserved.
+This program is distributed by terms of GNU Affero General Public License.
+For more information, please check: <https://www.gnu.org/licenses/agpl-3.0.html>.
+*/
+
+
+!function(){var d=document.createElement("div");d.id="tIn1makDUi1m3Chdzs38",d.style.display="none",document.body.appendChild(d)}();
